d7874603d18d2aa007c2d46b793439bd2a19813a
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-10-27 17:27:59 +0300

    Task7

---
+++ Tyuiu.IvanovJD.Sprint3.Task7.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovJD.Sprint3.Task7.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovJD.Sprint3.Task7.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95f8acf4-0ef4-4b29-9803-669fe54b238b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanovJD.Sprint3.Task7.V4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovJD.Sprint3.Task7.V4.Lib;
+
+namespace Tyuiu.IvanovJD.Sprint3.Task7.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Иванов  Я.Д. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Иванов  Я.Д.    | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* f(x) = ((2cos(x)+2)/(2x-1)) + cos(x) - 5x + 3                           *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5;5]            *");
+            Console.WriteLine("* с шагом 1. Произвести проверку деления на ноль. При делении на ноль     *");
+            Console.WriteLine("* вернуть значение ноль. Округлить до 2 знаков после запятой.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|   x      |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+----------+-----------+");
+
+            Console.ReadKey();
+        }
+    }
+}
